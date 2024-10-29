a2a54ea91f886b1e87e06f6855b9b1100d4f6083
zi-ao <869822845@qq.com>
2024-10-30 00:22:45 +0800

    初始化

---
+++ rspack.config.ts
@@ -0,0 +1,109 @@
+import { resolve } from 'node:path';
+import { defineConfig } from '@rspack/cli';
+import { type RspackPluginFunction, rspack } from '@rspack/core';
+import { VueLoaderPlugin } from 'vue-loader';
+import UnoCSS from '@unocss/postcss';
+import AutoImport from 'unplugin-auto-import/rspack';
+import Components from 'unplugin-vue-components/rspack';
+
+// Target browsers, see: https://github.com/browserslist/browserslist
+const targets = ['chrome >= 87', 'edge >= 88', 'firefox >= 78', 'safari >= 14'];
+
+const _resolve = (...paths: string[]) => resolve(__dirname, ...paths);
+
+export default defineConfig({
+  context: __dirname,
+  entry: {
+    main: _resolve('./src/main.ts'),
+  },
+  resolve: {
+    extensions: ['...', '.ts', '.vue'],
+    alias: {
+      '@': _resolve('./src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          experimentalInlineMatchResource: true,
+        },
+      },
+      {
+        test: /\.(js|ts)$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              sourceMap: true,
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                },
+              },
+              env: { targets },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [UnoCSS()],
+              },
+            },
+          },
+        ],
+        type: 'css',
+      },
+      {
+        test: /\.svg/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+    }),
+    new rspack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+    }),
+    new VueLoaderPlugin() as RspackPluginFunction,
+    AutoImport({
+      dts: './.temp/auto-imports.d.ts',
+      imports: ['vue'],
+    }),
+    Components({
+      dts: './.temp/components.d.ts',
+      resolvers: [
+        {
+          type: 'component',
+          name: (name: string) => {
+            if (name.match(/^(C[A-Z]|c-[a-z])/)) {
+              return { from: `@/components/${name.replace(/([A-Za-z])([A-Z])/g, '$1-$2').toLowerCase()}` };
+            }
+          },
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new rspack.SwcJsMinimizerRspackPlugin(),
+      new rspack.LightningCssMinimizerRspackPlugin({
+        minimizerOptions: { targets },
+      }),
+    ],
+  },
+  experiments: {
+    css: true,
+  },
+});
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="min-h-100vh">
+    Hello Word!
+  </div>
+</template>

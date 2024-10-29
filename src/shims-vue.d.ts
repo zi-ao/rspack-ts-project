d2fce4a34dea5bd2a0aae04163f076b368de3bca
zi-ao <869822845@qq.com>
2024-10-30 00:35:22 +0800

    添加 simple-git-hooks 和 lint-staged 并删除 eslint 和 prettier

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,6 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
+  // biome-ignore lint/complexity/noBannedTypes lint/suspicious/noExplicitAny: .vue 文件 TS 支持
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
